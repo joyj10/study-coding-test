3e977d3353e2bb904ab9e8e063acde39a1a1678e
dev_one <micool1030@gmail.com>
2024-04-20 22:24:39 +0900

    [solve] 그래프 탐색 : 연결 요소의 개수, 11724

--- src/graph/boj11724/Main.java
+++ src/graph/boj11724/Main.java
@@ -1,33 +1,69 @@
-package bruteforce.boj1260;
-
+package graph.boj11724;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
-// DFS와 BFS www.acmicpc.net/problem/1260
+// 연결 요소의 개수, www.acmicpc.net/problem/11724
 public class Main {
-
-    static int n, m, v;
+    static int n, m;
     static int[][] graph;
     static boolean[] visited;
-
+    static int count = 0;
 
     public static void main(String[] args) throws IOException {
         input();
-        dfs(v);
 
-        System.out.println();
-        visited = new boolean[n + 1];
-        bfs(v);
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                bfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 
+    // 스택을 이용한 dfs
     static void dfs(int node) {
+        Stack<Integer> s = new Stack<>();
+        s.push(node);
         visited[node] = true;
-        System.out.print(node + " ");
+
+        while (!s.isEmpty()) {
+            int now = s.pop();
+            for (int i = 1; i <=n; i++) {
+                if (graph[now][i] == 1 && !visited[i]) {
+                    s.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+
+    // 큐를 이용한 bfs
+    static void bfs(int node) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+        visited[node] = true;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            for (int i = 1; i <=n; i++) {
+                if (graph[now][i] == 1 && !visited[i]) {
+                    q.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+
+    // 재귀를 이용항 dfsRecursion
+    static void dfsRecursion(int node) {
+        visited[node] = true;
 
         for (int i = 1; i <= n; i++) {
             if (graph[node][i] == 1 && !visited[i]) {
@@ -36,41 +72,23 @@ public class Main {
         }
     }
 
-    static void bfs(int node) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(node);
-        visited[node] = true;
-
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            System.out.print(now + " ");
-            for (int i = 0; i <= n; i++) {
-                if (graph[now][i] == 1 && !visited[i]) {
-                    q.offer(i);
-                    visited[i] = true;
-                }
-            }
-        }
-
-    }
-
     static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
 
         graph = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int src = Integer.parseInt(st.nextToken());
-            int dst = Integer.parseInt(st.nextToken());
-            graph[src][dst] = 1;
-            graph[dst][src] = 1;
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            graph[x][y] = 1;
+            graph[y][x] = 1;
         }
     }
 }
